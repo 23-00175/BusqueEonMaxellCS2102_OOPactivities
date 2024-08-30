a99cc5ef327bb99ae158bca9ea15cebecd2d5b82
Eon Maxell Busque <165742990+23-00175@users.noreply.github.com>
2024-08-30 13:56:57 +0800

    Add files via upload

---
+++ Exercise Set 01/Tempconvert.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Tempconvert {
+    public static void main(String[] args) {
+        System.out.println("Temperature Converter");
+        Scanner myObj = new Scanner(System.in);
+        System.out.print("Enter a Celsius value: ");
+     
+        float celsius = myObj.nextFloat();
+        float fahrenheit = (celsius * 9) /5 + 32;
+        System.out.print(String.format("%.2f Celsius", celsius) + " is ");
+        System.out.print(String.format("%.2f Fahrenheit", fahrenheit));
+    }
+}
