312301c8a108d741bd15ae88a0e3dd877887a708
Eon Maxell Busque <165742990+23-00175@users.noreply.github.com>
2024-09-06 14:08:55 +0800

    Update GettingGreater.java

--- BusqueEonMaxellCS2102_OOPactivities/Laboratory Activities/Laboratory Activity 1 - 06-09-2024/GettingGreater.java
+++ BusqueEonMaxellCS2102_OOPactivities/Laboratory Activities/Laboratory Activity 1 - 06-09-2024/GettingGreater.java
@@ -10,11 +10,11 @@ public class GettingGreater {
         System.out.print("Enter second character: ");
         char char_2nd = myObj.next().charAt(0);
 
-        System.out.println("--------------------------");
+        System.out.println("------------------------------------------");
         System.out.println("The character with greater value is: " + (char) Math.max(char_1st, char_2nd));
-        System.out.println("--------------------------");
+        System.out.println("------------------------------------------");
         System.out.println("Showing the ASCII Codes");
-        System.out.println("--------------------------");
+        System.out.println("------------------------------------------");
         System.out.println(char_1st + " : " + (int) char_1st);
         System.out.println(char_2nd + " : " + (int) char_2nd);
     }
