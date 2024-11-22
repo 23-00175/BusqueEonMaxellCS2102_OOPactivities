7d243d2e7e840cda030fc844315cdbe9e41ae561
Eon Maxell Busque <165742990+23-00175@users.noreply.github.com>
2024-11-22 16:09:30 +0800

    Update ArtistDemo.java

--- BusqueEonMaxellCS2102_OOPactivities/Laboratory Activities/Laboratory Activity 3 - 11-22-2024/Artists/ArtistDemo.java
+++ BusqueEonMaxellCS2102_OOPactivities/Laboratory Activities/Laboratory Activity 3 - 11-22-2024/Artists/ArtistDemo.java
@@ -1,35 +1,28 @@
+
 public class ArtistDemo {
 
     public static void main(String[] args) {
 
         // instance of an artist 
         Artist artist = new Artist("Charlie Puth", "American", 32, "Music");
-        artist.displayInfo();
-
-        System.out.println();
 
         // instance of a singer 
-        Singer singer = new Singer("Nayeon Im", "Korean", 29, "Music", Singer.Type.POP);
-        singer.displayInfo();
-
-        System.out.println();
+        Artist singer = new Singer("Nayeon Im", "Korean", 29, "Music", Singer.Type.POP);
 
         // instance of a painter 
-        Painter painter = new Painter("Godwin Miles", "Filipino", 45, "Art", Painter.Type.CHARCOAL);
-        painter.displayInfo();
-
-        System.out.println();
+        Artist painter = new Painter("Godwin Miles", "Filipino", 45, "Art", Painter.Type.CHARCOAL);
 
         // instance of a writer
-        Writer writer = new Writer("Genesis Jim", "Filipino", 65, "Writing", Writer.Type.DRAMA);
+        Artist writer = new Writer("Genesis Jim", "Filipino", 65, "Writing", Writer.Type.DRAMA);
+
+        // instance of a dancera
+        Artist dancer = new Dancer("Hubert Ross", "Filipino", 69, "Dancing", Dancer.Type.HIPHOP);
+
+        artist.displayInfo();
+        singer.displayInfo();
+        painter.displayInfo();
         writer.displayInfo();
-
-        System.out.println();
-
-        // instance of a dancer
-        Dancer dancer = new Dancer("Hubert Ross", "Filipino", 69, "Dancing", Dancer.Type.HIPHOP);
         dancer.displayInfo();
 
-        System.out.println();
     }
 }
